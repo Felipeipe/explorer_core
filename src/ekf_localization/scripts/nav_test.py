23a06fa14197eff8f9dc5c4f5273f94e3a703fa7
Felipe González <gonzalezpobletefelipe@gmail.com>
2025-06-15 19:27:05 -0400

    added amcl trajectory

--- src/ekf_localization/scripts/nav_test.py
+++ src/ekf_localization/scripts/nav_test.py
@@ -2,7 +2,7 @@
 import rclpy
 from rclpy.node import Node
 import matplotlib.pyplot as plt
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
 from sensor_msgs.msg import Joy
 from tf_transformations import euler_from_quaternion
 import os
@@ -16,7 +16,14 @@ class OdometryPlotter(Node):
             PoseStamped,
             '/ground_truth_pose',
             self.gt_callback,
-            10)
+            10
+        )
+        self.estimation_sub = self.create_subscription(
+            PoseWithCovarianceStamped,
+            '/amcl_pose',
+            self.estimation_callback,
+            10
+        )
         self.joy_sub = self.create_subscription(
             Joy,
             '/joy',
@@ -28,7 +35,8 @@ class OdometryPlotter(Node):
         self.y = 0.0
         self.theta = 0.0
         self.plot = False
-        self.positions = []
+        self.ground_truth = []
+        self.estimation = []
 
     def gt_callback(self,msg):
         self.x = msg.pose.position.x
@@ -41,23 +49,35 @@ class OdometryPlotter(Node):
         ]
 
         _,_,self.theta = euler_from_quaternion(orientation)
-        self.positions.append([self.x, self.y, self.theta])
+        self.ground_truth.append([self.x, self.y, self.theta])
+    def estimation_callback(self, msg:PoseWithCovarianceStamped):
+        self.x = msg.pose.pose.position.x
+        self.y = msg.pose.pose.position.y
+        orientation = [
+            msg.pose.pose.orientation.x,
+            msg.pose.pose.orientation.y,
+            msg.pose.pose.orientation.z,
+            msg.pose.pose.orientation.w
+        ]
+
+        _,_,self.theta = euler_from_quaternion(orientation)
+        self.estimation.append([self.x, self.y, self.theta])
+
 
     def joy_callback(self,msg):  
         if msg.buttons[7] == 1 and not self.plot:
             self.plot = True 
             self.plot_odometry()
     def plot_odometry(self):
-        if not self.positions:
-            self.get_logger().info("No hay datos de odometría para graficar.")
-            return
-        x_vals, y_vals, _ = zip(*self.positions)  # Descomprimir listas x e y
-        
+        gt_x, gt_y, gt_theta = zip(*self.ground_truth)  # Descomprimir listas x e y
+        est_x, est_y, est_theta = zip(*self.estimation)
+
         plt.figure(figsize=(6, 6))
-        plt.plot(x_vals, y_vals, color='b', label="Trayectoria")
+        plt.plot(gt_x, gt_y, label="Trayectoria real del robot")
+        plt.plot(est_x, est_y, label='Trayectoria según AMCL (sin calibrar)')        
         plt.xlabel("X (m)")
         plt.ylabel("Y (m)")
-        plt.title("Trayectoria del Robot")
+        plt.title("Trayectoria real vs AMCL sin calibrar")
         plt.legend(loc='best')
         plt.grid(True)
         plt.savefig('Ground_truth_trajectory.png')
